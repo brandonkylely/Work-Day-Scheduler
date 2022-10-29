b862abeff113337ce9451c81cfd4d1eeb07d809a
Brandon <brandonkly@ucla.edu>
2022-10-28 20:06:33 -0700

    started work on save function, changed structure in html

--- assets/script.js
+++ assets/script.js
@@ -5,33 +5,56 @@ var date = new Date();
 // console.log(date)
 // console.log(date.getHours())
 // console.log(moment(hours[17], " HH").format('h a'))
+var events = [];
+var work ={}
+localStorage.getItem("events", JSON.stringify(events));
+
+
+
 
 function referenceTime (hour) {
     if (date.getHours() === hour) {
-        return 1 // present
+        return "present"
     } else if (date.getHours() > hour) {
-        return 2 //past
+        return "past"
     } else {
-        return 3 //future
+        return "future"
     };
 }
 
+// function saveEvent() {
+//     work.text = textbox.value
+//     work.hour = 
+//     events.push(work);
+//     textarea.value = localStorage.setItem("events", JSON.parse(events));
+// }
+
 for (var i = 0; i < hours.length; i++) {
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "row timeblock");
-    if (referenceTime(hours[i]) === 1) {
-        wrapper.classList.add("present")
-    } else if (referenceTime(hours[i]) === 2)  {
-        wrapper.classList.add("past")
+
+    // var wrapper = document.createElement("div");
+    // wrapper.setAttribute("class", "row timeblock");
+    var block = document.querySelectorAll(".timeblock")
+
+    if (referenceTime(hours[i]) === "present") {
+        block[i].classList.add("present")
+    } else if (referenceTime(hours[i]) === "past")  {
+        block[i].classList.add("past")
     } else {
-        wrapper.classList.add("future")
+        block[i].classList.add("future")
     }
     var hourDisplay = document.createElement("div");
+
     var textbox = document.createElement("textarea");
     textbox.setAttribute("class", "description col-md-10");
+
     hourDisplay.setAttribute("class", "hour");
     hourDisplay.textContent = moment(hours[i], "HH").format('h a');
-    wrapper.appendChild(hourDisplay);
-    wrapper.appendChild(textbox);
-    container.appendChild(hourDisplay);
+    block[i].appendChild(hourDisplay);
+    block[i].appendChild(textbox);
+    container.appendChild(block[i]);
+
+    // var saveButton = document.createElement("button")
+    // saveButton.addEventListener("click", saveEvent)
+
+    console.log(block.classList)
 }
