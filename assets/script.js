04580c535ec64334a38eb68b26c45ac9b4126d99
Brandon <brandonkly@ucla.edu>
2022-10-28 18:50:55 -0700

    made hours array start at 0, 12am

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,20 @@
 var container = document.querySelector(".container");
 // var hours = ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
-var hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+var hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 var date = new Date();
-// function referenceTime (hour) {
-//     if (date.getHours() === hour) {
+// console.log(date)
+// console.log(date.getHours())
+// console.log(moment(hours[17], " HH").format('h a'))
 
-//     } else if {
+function referenceTime (hour) {
+    if (date.getHours() === hour) {
 
-//     } else {
+    } else if (date.getHours() > hour) {
 
-//     };
-// }
+    } else {
+
+    };
+}
 
 for (var i = 0; i < hours.length; i++) {
     var wrapper = document.createElement("div");
